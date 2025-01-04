07910715d7f126e384467874782507b5a65e6fba
Ladislao Bordon <ladislao.bordon@gmail.com>
2025-01-04 00:27:55 -0300

    primeros cambios

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,4 +1,11 @@
-import { useState } from 'react'
+import Titulo from "./components/titulo";
+function App() {
+  return <Titulo />;
+}
+
+export default App;
+
+/*import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -29,7 +36,4 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
-}
-
-export default App
+  )*/
---
+++ react-app/src/components/Titulo.tsx
@@ -0,0 +1,10 @@
+function Titulo() {
+  return (
+    <div>
+      <h1>React Titulo</h1>
+      <p>Open the console to see the output of the Rust function.</p>
+    </div>
+  );
+}
+
+export default Titulo;
