9cf7e0a5c16260cbbad6f704416b3fbf8d2fbc60
Ladislao Bordon <ladislao.bordon@gmail.com>
2025-03-22 22:22:26 -0300

    CRUD

--- FastApi/users.py
+++ FastApi/users.py
@@ -25,4 +25,46 @@ async def users():
 @app.get("/user/{id}")
 async def user(id: int):
     users = filter(lambda user: user.id == id, users_list)
-    return list(users)[0]
+    try:
+        return list(users)[0]
+    except:
+        return {"message": "User not found"}
+
+@app.get("/userquery/")
+async def userquery(id: int):
+    users = filter(lambda user: user.id == id, users_list)
+    try:
+        return list(users)[0]
+    except:
+        return {"message": "User not found"}
+    
+@app.post("/user")
+async def create_user(user: User):
+    if type(search_user(user.id)) == User:
+        return {"message": "User already exists"}
+    else:
+        users_list.append(user)
+    return user
+
+@app.put("/user")
+async def update_user(user: User):
+    for index,saved_user in enumerate(users_list):
+        if saved_user.id == user.id:
+            users_list[index] = user
+            return user
+    return {"message": "User not found"}
+
+@app.delete("/user/{id}")
+async def delete_user(id:int):
+    for index,saved_user in enumerate(users_list):
+        if saved_user.id == id:
+            del users_list[index]
+            return {"message" : "User succesfully deleted"}
+    return {"message": "User not found"}
+
+def search_user(id: int):
+    users = filter(lambda user: user.id == id, users_list)
+    try:
+        return list(users)[0]
+    except:
+        return {"message": "User not found"}
